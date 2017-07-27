a77041ff3459d78e100a0021a2bc4ba3db8cadc2
wanls4583 <409051987@qq.com>
2017-07-27 23:23:14 +0800

    add file

---
+++ js/common.js
@@ -0,0 +1,19 @@
+layer && layer.config({
+    extend: ['skin/osa/style.css'], //加载新皮肤
+    skin: 'layer-ext-osa' //一旦设定，所有弹层风格都采用此主题。
+});
+Array.prototype.indexOf = function(val) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == val) return i;
+	}
+	return -1;
+};
+Array.prototype.remove = function(val) {
+	var index = this.indexOf(val);
+	if (index > -1) {
+		this.splice(index, 1);
+	}
+};
+var globalDatas = {};
+globalDatas.configFilePath = "data/config.txt";
+globalDatas.taskFilePath = "data/task.txt";
---
+++ js/menu.js
@@ -0,0 +1,20 @@
+var menu = {
+	'project': {
+		title: '项目',
+		menu: [{
+			title: '项目',
+			icon: 'imgs/main/l03.png',
+			href: 'list.html',
+			isCurrent: true
+		}]
+	},
+	'config': {
+		title: '配置',
+		menu: [{
+			title: '配置',
+			icon: 'imgs/main/l17.png',
+			href: 'config.html',
+			isCurrent: true
+		}]
+	}
+};
